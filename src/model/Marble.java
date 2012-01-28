8f5bb0ad48ead4f16a0deb518856d4c6a9882f05
Emmanuel Corno <emmanuel.corno@gmail.com>
2012-01-28 01:13:56 +0100

    Update class Marble

--- src/model/Marble.java
+++ src/model/Marble.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Marble {
+import java.awt.Point;
 
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import utility.IVisible;
+
+public class Marble implements IVisible
+{
+	/// ATTRIBUTES
+	private Point position;
+	private Image image;
+	
+	public Marble()
+	{
+		try {
+			image = new Image("marbel.PNG");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(image, position.x, position.y);
+		
+	}
+	
 }
