e33c23057d94ce0a7eb05803f229577b5e25c690
Emmanuel Corno <emmanuel.corno@gmail.com>
2012-01-29 00:53:07 +0100

    New class : a TRUE input controller...

---
+++ src/system/TrueJInputController.java
@@ -0,0 +1,196 @@
+package system;
+
+import org.lwjgl.input.Controller;
+
+public class TrueJInputController implements Controller {
+
+	
+	
+	
+	@Override
+	public int getAxisCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAxisName(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getAxisValue(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getButtonCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getButtonName(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getDeadZone(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getPovX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getPovY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRXAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRXAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRYAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRYAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRZAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRZAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getXAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getXAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getYAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getYAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getZAxisDeadZone() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getZAxisValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isButtonPressed(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void poll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDeadZone(int arg0, float arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRXAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRYAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRZAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setXAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setYAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setZAxisDeadZone(float arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
