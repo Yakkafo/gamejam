9b2b29c69687d57ddf726e327b1fcbe6aadbd91d
William <wilbefast@gmail.com>
2012-01-28 01:19:23 +0100

    center snake

---
+++ src/model/CenterSnake.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.awt.Point;
+
+public class CenterSnake extends SnakeHead
+{
+	/// CONSTANTS
+	private static final double RADIUS = 128;
+	
+	/// METHODS
+	
+	// creation
+	public CenterSnake(Point init_center)
+	{
+		super(init_center, RADIUS);
+	}
+
+}
--- src/system/Game.java
+++ src/system/Game.java
@@ -1,18 +1,29 @@
 package system;
  
+import java.awt.Dimension;
+import java.awt.Point;
+
+import model.CenterSnake;
 import model.SnakeHead;
 
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.AppGameContainer;
  
 public class Game extends BasicGame 
 {
+	/// CONSTANTS
+	private static final int DESIRED_W = 1024;
+	private static final int DESIRED_H = 768;
+	private static final boolean USE_FULLSCREEN = false;
+	private static final int MAX_FPS = 30;
+	
 	/// ATTRIBUTES
-	//SnakeHead inner_snake = new SnakeHead();
+	private Dimension size;
+	private SnakeHead inner_snake;
+	private int frames_since_update = 0;
 	
 	/// METHODS
 	
@@ -24,13 +35,24 @@ public class Game extends BasicGame
 	@Override
 	public void init(GameContainer container) throws SlickException
 	{
+		size = new Dimension(container.getWidth(), container.getHeight());
+		
+		// Snakes centered on middle of screen
+		Point middle = new Point(size.width/2, size.height/2);
+		inner_snake = new CenterSnake(middle);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException
 	{
+		// Regulate framerate
+		frames_since_update += delta;
+		if(frames_since_update < 1000/MAX_FPS)
+			return;
+		frames_since_update = 0;
 		
+		inner_snake.addAngle(0.3);
 	}
 
 	@Override
@@ -45,7 +67,7 @@ public class Game extends BasicGame
 		try
 		{
 			AppGameContainer app = new AppGameContainer(new Game());
-			app.setDisplayMode(1024, 720, false);
+			app.setDisplayMode(DESIRED_W, DESIRED_H, USE_FULLSCREEN);
 			app.start();
 		}
 		catch (SlickException e)
